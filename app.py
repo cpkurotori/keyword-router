8ce1768dd51e51f0e57913dc8d0a532f1dce02d3
Cameron Kurotori <cameron.kurotori@ibm.com>
2020-04-11 17:31:11 -0500

    fix to work with heroku and add onsite clues

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template
 from flask import request, redirect
 import yaml
-import os
 
 with open("clues.yaml") as file:
     raw_yaml_file = next(yaml.safe_load_all(file))
 # print(list(raw_yaml_file))
-keyword_map = {item["keyword"]: item["url"] for item in raw_yaml_file}
+keyword_map = {item["keyword"]: item["next"] for item in raw_yaml_file}
 
 app = Flask(__name__)
 
@@ -14,13 +13,20 @@ app = Flask(__name__)
 def index():
     return render_template("index.html", invalid=False)
 
+def handle_keyword(keyword):
+    if keyword not in keyword_map:
+        return render_template("index.html", invalid=True)
+    next = keyword_map[keyword]
+    if next["type"].lower() == "url":
+        return redirect(next["value"])
+    if next["type"].lower() == "clue":
+        return render_template("clue.html", clue=next["value"])
+
+
 @app.route("/", methods=["POST"])
 def get_keywords():
     keyword = request.form.get("keyword")
-    if keyword and keyword in keyword_map:
-        return redirect(keyword_map[keyword])
-    else:
-        return render_template("index.html", invalid=True)
+    return handle_keyword(keyword)
 
 if __name__ == "__main__":
-    app.run(host=os.getenv("HOST", "0.0.0.0"), port=os.getenv("PORT", "5000"))
+    app.run()
